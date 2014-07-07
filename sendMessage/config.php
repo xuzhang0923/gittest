421c4ad6878cf795e572190922ab982b084c46f8
xuzhang.microstrategy.com <xuzhang@microstrategy.com>
2014-07-07 11:00:55 +0800

    test
    this is a test

---
+++ sendMessage/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    define("DB_HOST","localhost");
+    define("DB_USER","root");
+    define("DB_PASSWORD","");
+    define("DB_DATABASE","gcm");
+    define("GOOGLE_API_KEY","AIzaSyDgNDUBZMJzlD33LEn6dOQhvrqY2w0YVJ4");
+    
+    define('SALT1','PASSWORD1');
+    define('SALT2','PASSWORD2');
+    
+    session_start();
+    
+    $_session['error']="";
+	
+	//I use this for test, let see what will happens.
+?>
